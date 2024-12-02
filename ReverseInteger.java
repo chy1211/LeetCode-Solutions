a45b584b518da9a1c381f2071a790f2d75c08b3b
chy1211 <henry70304@gmail.com>
2024-12-03 03:46:42 +0800

    Day5 #7

--- ReverseInteger.java
+++ ReverseInteger.java
@@ -4,10 +4,11 @@ public class ReverseInteger {
         while (x != 0) {
             int pop = x % 10;
             x = x / 10;
-            // int 的最大值為 2147483647，最小值為 -2147483648
+            // int 的最大值為 2147483647
             // 如果 reversed > Integer.MAX_VALUE/10，那麼 reversed * 10 + pop 會溢位
             // 如果 reversed == Integer.MAX_VALUE/10 且 pop > 7，那麼 reversed * 10 + pop 會溢位
             if (reversed > Integer.MAX_VALUE/10 || (reversed == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            // int 的最小值為 -2147483648
             // 如果 reversed < Integer.MIN_VALUE/10，那麼 reversed * 10 + pop 會溢位
             // 如果 reversed == Integer.MIN_VALUE/10 且 pop < -8，那麼 reversed * 10 + pop 會溢位
             if (reversed < Integer.MIN_VALUE/10 || (reversed == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
