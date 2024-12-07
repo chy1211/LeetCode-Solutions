7b748b441886cdfa8ca15a29eca5fbfa5aa72bd6
chy1211 <henry70304@gmail.com>
2024-12-07 08:20:47 +0800

    Day9 #11

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,26 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+
+            // 移動指針矮的那一邊
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(obj.maxArea(height));
+    }
+}
